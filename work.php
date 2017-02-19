a57e34e53cb744c62a8d072ebca823390a28bd3e
mglezh <mglezh@gmail.com>
2017-02-19 22:39:59 +0100

    Base de datos 3
    Se modificó work.php para que funcione con base de datos

--- work.php
+++ work.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+<? require_once("php/datalogin.php"); ?>
 <html lang="en">
 	<head>
 		<meta charset="utf-8">    
@@ -15,7 +16,6 @@
 	    <link href="css/jquery.bxslider.css" rel="stylesheet" />
 	    
 	   	<script type="text/javascript" src="js/work.js"></script>
-
 		<script defer type="text/javascript" src="js/codigo.js"></script>
 	</head>
 	<body>
@@ -28,86 +28,36 @@
 		    </nav>
 			<section class="works">
 			    <ul>
-					<li>
-						<p class="Title" onmouseover="SelectSlider(8)" >
-						Desarrollo de farolas solares. Sistemas de telegestión. </p>
-						<p>(2015-...). EKIONA, Iluminación Solar. Guipuzcoa. España </p>
-					</li>
-					<li>
-						<p class="Title" onmouseover="SelectSlider(4)" >
-						Desarrollo de prototipos funcionales de los bloques de amplificadores y estimuladores somato-sensorial para la monitorización intraoperatoria. </p>
-						<p>(2011-2014). Centro de Neurociencias de Cuba (CNEURO). Cuba </p>
-					</li>
-					<li>
-						<p class="Title" onmouseover="SelectSlider(6)" >
-						PALMIBER: Investigación, desarrollo y construcción de un vehículo eléctrico especial configurable para el apoyo a la movilidad aumentativa de niños con deficiencias neuromotoras graves. </p>
-						<p> Proyecto IBEROEKA (IBK 02-277). </p>
-						<p> (2005-2007). IC NEURONIC. Zaragoza. España </p>
-						<p> (2008-2011). Centro de Neurociencias de Cuba (CNEURO). Cuba </p>
-					</li>
-					<li>
-						<p class="Title" onmouseover="SelectSlider(4)" >
-						Desarrollo de nuevas aplicaciones para los equipos de potenciales evocados y electromiografía: Fibra Simple y P300. </p>
-						<p>(2008). Centro de Neurociencias de Cuba (CNEURO). Cuba </p>
-					</li>
-					<li>
-						<p class="Title" onmouseover="SelectSlider(2)" >
-						Desarrollo de herramientas (software y hardware) a emplear en el proceso productivo de equipos de Neurofisiología. </p>
-						<p> (2005-2007). IC NEURONIC. Zaragoza. España </p>
-						<p> (2007-2011). Centro de Neurociencias de Cuba (CNEURO). Cuba </p>
-					</li>
-					<li>
-						<p class="Title" onmouseover="SelectSlider(2)" >
-						Diseño de la 5ta Generación de equipos de Neurofisiología </p>
-						<p> (Medicid 5) Polígrafo digital de 36 canales de registro.  </p>
-						<p> (Audix 5) Electroaudiómetro automatizado.  </p>
-						<p> (Neuronica 5) Equipo de potenciales evocados y electromiografía. </p>
-						<p> (Medicid 5-68) Polígrafo digital de alta densidad, 68 canales de registro. </p>
-						<p> (Medicid 5-132) Polígrafo digital de alta densidad, 132 canales de registro. </p>
-						<p> Registro médico en Cuba.</p>
-						<p> Obtención de certificación Europea (Marcado CE). </p>
-						<p> (2005-2007). IC NEURONIC. Zaragoza. España </p>
-						<p> (1998-2008). Centro de Neurociencias de Cuba (CNEURO). Cuba </p>
-					</li>
-					<li>
-						<p class="Title" onmouseover="SelectSlider(5)" >
-						Diseño de un sistema de EEG con 128 canales de registro. </p>
-						<p> (1997-1998). Centro Nacional de Investigaciones Científicas (CNIC). Cuba </p>
-					</li>
-					<li>
-						<p class="Title" onmouseover="SelectSlider(2)" >
-						Diseño de un sistema para la calibración de señales auditivas. </p>
-						<p> (1997). Centro Nacional de Investigaciones Científicas (CNIC). Cuba </p>
-					</li>
-					<li>
-						<p class="Title" onmouseover="SelectSlider(0)" >
-						Diseño de la 4ta Generación de equipos de Neurofisiología </p>
-						<p> (Medicid 4, Polígrafo digital; Audix, Electroaudiómetro automatizado; Neuronica 4, Equipo de potenciales evocados y electromiografía. </p>
-						<p> Registro médico en Cuba. </p>
-						<p> Obtención de certificación Europea (Marcado CE). </p>
-						<p> (1993-1998). Centro Nacional de Investigaciones Científicas (CNIC). Cuba </p>
-					</li>
-					<li>
-						<p class="Title" onmouseover="SelectSlider(0)" >
-						Diseño de una tarjeta de 16 canales para la adquisición de señales (NX1) electrofisiológicas.. </p>
-						<p Registro médico en Cuba. </p>
-						<p> (1992-1995). Centro Nacional de Investigaciones Científicas (CNIC). Cuba </p>
-					</li>
+		          	<?
+		                $query = "SELECT * FROM `trabajos` where `titulo?`='1'";
+		                $result = mysqli_query($db, $query);
+		                while($row = mysqli_fetch_assoc($result)) {
+		            ?>
+				    	<li>
+							<p class="Title" onmouseover="SelectSlider('<?=$row['slide#']?>')" >
+							<?=$row['titulo']?> </p>
+					          	<?
+					          		$trabajo = $row['trabajo#'];
+					                $query = "SELECT * FROM `trabajos` WHERE `trabajo#`=".$trabajo." AND `titulo?`='0'";
+					                $result2 = mysqli_query($db, $query);
+					                while($row2 = mysqli_fetch_assoc($result2)) {
+					            ?>
+									<p><?=$row2['titulo']?></p>
+					          	<?}?>
+				        </li>
+		          	<?}?>
 			    </ul>
 		    </section>
 	        <aside id = "sidebar">
 			    <section class="worksslider" id="worksslider">
 			        <ul class="bxslider" id="bxslider">
-						<li><img class="Works" src="images/Trabajos/Medicid 4_1.jpg" title="Medicid 4" /></li>
-						<li><img class="Works" src="images/Trabajos/Neuronica 4_1.jpg" title="Neuronica 4" /></li>
-						<li><img class="Works" src="images/Trabajos/Audix 5_1.jpg" title="Audix 5" /></li>
-						<li><img class="Works" src="images/Trabajos/Medicid 5_1.jpg" title="Medicid 5"/></li>
-						<li><img class="Works" src="images/Trabajos/Neuronica 5_1.jpg" title="Neuronica 5"/></li>
-						<li><img class="Works" src="images/Trabajos/Medicid 5_132.jpg" title="Medicid 5-132"/></li>
-						<li><img class="Works" src="images/Trabajos/Palmiber 1.jpg" title="Palmiber"/></li>
-						<li><img class="Works" src="images/Trabajos/Palmiber 2.jpg" title="Palmiber"/></li>
-						<li><img class="Works" src="images/Trabajos/EKIONA 1.jpg" title="EKIONA Farolas solares"/></li>
-						<li><img class="Works" src="images/Trabajos/EKIONA 2.jpg" title="EKIONA Telegestión"/></li>
+			          	<?
+			                $query = "SELECT * FROM `slider` ";
+			                $result = mysqli_query($db, $query);
+			                while($row = mysqli_fetch_assoc($result)) {
+			            ?>
+							<li><img class="Works" src="<?=$row['enlace']?>" title="<?=$row['titulo']?>"/></li>
+			          	<?}?>
 			        </ul>
 			    </section>
 			</aside>
